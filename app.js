fcce713723e17d287055dedcf560811147e80449
robeerob <rob@rob.ee>
2016-08-02 21:13:57 +0200

    Correct type controllers in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
 var setupController = require('./controllers/setupController');
-var apiController = require('./controller/apiController');
+var apiController = require('./controllers/apiController');
 
 var port = process.env.PORT || 3000;
 
