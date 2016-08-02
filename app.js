c80e731c11163a858380fed89ae78a91502b8b79
robeerob <rob@rob.ee>
2016-08-02 21:12:10 +0200

    Update app.js with apiController added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
 var setupController = require('./controllers/setupController');
+var apiController = require('./controller/apiController');
 
 var port = process.env.PORT || 3000;
 
@@ -12,5 +13,6 @@ app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString());
 setupController(app);
+apiController(app);
 
 app.listen(port);
